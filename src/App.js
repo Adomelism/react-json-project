5db3735ab6ae004251d28bfa2b128eff56b36a99
Adomelism <adomelism@gmail.com>
2023-06-19 10:27:16 +0300

    new project structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import UsersPage from './Pages/UsersPage/UsersPage';
+import UserPage from './Pages/UserPage/UserPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<nav className='main-navigation'>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <NavLink to='/' className='nav-link'>Home</NavLink>
+          </li>
+
+          <li className='nav-item'>
+            <NavLink to='/UsersPage' className='nav-link'>Users</NavLink>
+          </li>
+
+          <li className='nav-item'>
+            <NavLink to='/UserPage' className='nav-link'>User</NavLink>
+          </li>
+        </ul>
+      </nav>
+      
+      <Routes>
+            <Route path='/UsersPage' element={<UsersPage />} />
+            <Route path='/UserPage' element={<UserPage />} />
+
+            <Route path='/' element={ 
+              <div>
+                <h1>HomePage</h1>
+                <p>This is Home Page under construction...</p>
+              </div>
+              } />
+            <Route path='*' element={
+              <div>
+                <h1>404 error. Page not found.</h1>
+                <Link to='/'>Go Back to HomePage</Link>
+              </div>
+              } />
+        </Routes>
+
     </div>
   );
 }
