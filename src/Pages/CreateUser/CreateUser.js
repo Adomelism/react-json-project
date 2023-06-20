a0e6c74e8fd1312fad7af7ce5a7294eac0c0a1b0
Adomelism <adomelism@gmail.com>
2023-06-20 23:18:24 +0300

    create users function added

--- src/Pages/CreateUser/CreateUser.js
+++ src/Pages/CreateUser/CreateUser.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Container from "../../Components/Container/Container";
+import axios from "axios";
+import { API_URL } from "../../config";
+import { Link } from "react-router-dom";
 
 
 const CreateUser = () => {
 
+    const [user, setUser] = useState([])
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
@@ -18,6 +22,13 @@ const CreateUser = () => {
     const [companyName, setCompanyName] = useState('')
     const [catchPhrase, setCatchPhrase] = useState('')
     const [bs, setBs] = useState('')
+    const [createUser, setCreateUser] = useState(false)
+
+    useEffect(() => {
+        axios.get(`${API_URL}/users`).then((res) => {
+          setUser(res.data[0].id);
+        });
+      }, []);
 
     const nameHandler = (event) => setName(event.target.value)
     const usernameHandler = (event) => setUsername(event.target.value)
@@ -34,86 +45,120 @@ const CreateUser = () => {
     const catchPhraseHandler = (event) => setCatchPhrase(event.target.value)
     const bsHandler = (event) => setBs(event.target.value)
 
+    const createUserHandler = (event) => {
+        event.preventDefault();
+        setCreateUser(true);
+        fetch(`${API_URL}/users`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                username,
+                email,
+                address: { street: street, city: city, suite: suite, zipcode: zipcode, geo: { lat: lat, lng: lng } },
+                phone,
+                website,
+                company: { name: companyName, catchPhrase, bs },
+                userId: Number(user),
+                
+
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+    }
+    
+    
+
   return (
     <Container>
-        <form>
-            <div className="form-control">
-                <label htmlFor="name">name:</label>
-                <input onChange={nameHandler} value={name} type='text' id='name' name='name'></input>
-            </div>
 
-            <div className="form-control">
-                <label htmlFor="username">username:</label>
-                <input onChange={usernameHandler} value={username} type='text' id='username' name='username'></input>
-            </div>
+        {createUser ? <h1><Link to={`/usersPage`}>User was created. See list of users:</Link></h1> : (
 
-            <div className="form-control">
-                <label htmlFor="email">email:</label>
-                <input onChange={emailHandler} value={email} type='email' id='email' name='email'></input>
-            </div>
+<form onSubmit={createUserHandler}>
+<div className="form-control">
+    <label htmlFor="name">name:</label>
+    <input onChange={nameHandler} value={name} type='text' id='name' name='name'></input>
+</div>
 
-            <div className="form-control">
-                <h4>Address:</h4>
-                <label htmlFor="street">street:</label>
-                <input onChange={streetHandler} value={street} type='text' id='street' name='street'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="username">username:</label>
+    <input onChange={usernameHandler} value={username} type='text' id='username' name='username'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="suite">suite:</label>
-                <input onChange={suiteHandler} value={suite} type='text' id='suite' name='suite'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="email">email:</label>
+    <input onChange={emailHandler} value={email} type='email' id='email' name='email'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="city">city:</label>
-                <input onChange={cityHandler} value={city} type='text' id='city' name='city'></input>
-            </div>
+<div className="form-control">
+    <h4>Address:</h4>
+    <label htmlFor="street">street:</label>
+    <input onChange={streetHandler} value={street} type='text' id='street' name='street'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="zipcode">zipcode:</label>
-                <input onChange={zipcodeHandler} value={zipcode} type='number' id='zipcode' name='zipcode'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="suite">suite:</label>
+    <input onChange={suiteHandler} value={suite} type='text' id='suite' name='suite'></input>
+</div>
 
-            <div className="form-control">
-                <h4>Geo:</h4>
-                <label htmlFor="lat">lat:</label>
-                <input onChange={latHandler} value={lat} type='number' id='lat' name='lat'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="city">city:</label>
+    <input onChange={cityHandler} value={city} type='text' id='city' name='city'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="lng">lng:</label>
-                <input onChange={lngHandler} value={lng} type='number' id='lng' name='lng'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="zipcode">zipcode:</label>
+    <input onChange={zipcodeHandler} value={zipcode} type='number' id='zipcode' name='zipcode'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="phone">phone:</label>
-                <input onChange={phoneHandler} value={phone} type='phone' id='phone' name='phone'></input>
-            </div>
+<div className="form-control">
+    <h4>Geo:</h4>
+    <label htmlFor="lat">lat:</label>
+    <input onChange={latHandler} value={lat} type='number' id='lat' name='lat'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="website">website:</label>
-                <input onChange={websiteHandler} value={website} type='text' id='website' name='website'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="lng">lng:</label>
+    <input onChange={lngHandler} value={lng} type='number' id='lng' name='lng'></input>
+</div>
 
-            <div className="form-control">
-                <h4>Company:</h4>
-                <label htmlFor="CompanyName">name:</label>
-                <input onChange={companyNameHandler} value={companyName} type='text' id='CompanyName' name='CompanyName'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="phone">phone:</label>
+    <input onChange={phoneHandler} value={phone} type='phone' id='phone' name='phone'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="catchPhrase">catch phrase:</label>
-                <input onChange={catchPhraseHandler} value={catchPhrase} type='text' id='catchPhrase' name='catchPhrase'></input>
-            </div>
+<div className="form-control">
+    <label htmlFor="website">website:</label>
+    <input onChange={websiteHandler} value={website} type='text' id='website' name='website'></input>
+</div>
 
-            <div className="form-control">
-                <label htmlFor="bs">bs:</label>
-                <input onChange={bsHandler} value={bs} type='text' id='bs' name='bs'></input>
-            </div>
+<div className="form-control">
+    <h4>Company:</h4>
+    <label htmlFor="CompanyName">name:</label>
+    <input onChange={companyNameHandler} value={companyName} type='text' id='CompanyName' name='CompanyName'></input>
+</div>
 
-            
+<div className="form-control">
+    <label htmlFor="catchPhrase">catch phrase:</label>
+    <input onChange={catchPhraseHandler} value={catchPhrase} type='text' id='catchPhrase' name='catchPhrase'></input>
+</div>
 
-            <input type='submit' value='create new user'></input>
-        </form>
+<div className="form-control">
+    <label htmlFor="bs">bs:</label>
+    <input onChange={bsHandler} value={bs} type='text' id='bs' name='bs'></input>
+</div>
+
+
+
+<input type='submit' value='create new user'></input>
+</form>
+
+        )}
+       
     </Container>
   )
 }
