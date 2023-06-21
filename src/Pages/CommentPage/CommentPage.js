1f26c1542626ed350ae6ab6007866d96fb17b0e4
Adomelism <adomelism@gmail.com>
2023-06-21 09:57:08 +0300

    added delete functionality to comments

--- src/Pages/CommentPage/CommentPage.js
+++ src/Pages/CommentPage/CommentPage.js
@@ -2,12 +2,13 @@ import Container from '../../Components/Container/Container';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { API_URL } from '../../config';
+import { Link } from 'react-router-dom';
 
 const CommentPage = () => {
 
   const { id } = useParams();
-
   const [comment, setComment] = useState(null)
+  const [deleteComment, setDeleteComment] = useState(false)
 
   useEffect(() => {
     fetch(`${API_URL}/comments/${id}`)
@@ -18,15 +19,31 @@ const CommentPage = () => {
     })
   }, [id])
 
+  const deleteHandler = (event) => {
+    event.preventDefault();
+    setDeleteComment(true);
+    fetch(`${API_URL}/comments/${id}`, {
+  method: 'DELETE',
+});
+  }
+
   if (!comment) {
     return '';
   }
 
   return (
     <Container>
-      <h1>Author: {comment.email}</h1>
-      <h2>Headline: {comment.name}</h2>
-      <h3>Comment: {comment.body}</h3>
+      {deleteComment ? <h1><Link to={`/commentsPage`}>Comment was deleted. See rest of the comments:</Link></h1> : (
+        <div>
+          <h1>Author: {comment.email}</h1>
+          <h2>Headline: {comment.name}</h2>
+          <h3>Comment: {comment.body}</h3>
+          <button onClick={deleteHandler}>Delete</button>
+        </div>
+      
+
+      )}
+
     </Container>
   )
 }
